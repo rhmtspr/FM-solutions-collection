f6b3e452e1e9acabb3895fd2343f9d955e46fa01
rachmatrisky <rachmatrisky5@gmail.com>
2024-03-29 15:18:01 +0800

    make card list responsive

--- social-media-dashboard-with-theme-switcher-master/resources/js/script.js
+++ social-media-dashboard-with-theme-switcher-master/resources/js/script.js
@@ -6,17 +6,17 @@ toggleSwitch.addEventListener('change', switchTheme, false);
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
 
-  if (currentTheme === 'dark') {
+  if (currentTheme === 'light') {
     toggleSwitch.checked = true;
   }
 }
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   }
 }
